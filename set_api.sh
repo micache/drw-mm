19127abec0b73289c9df042d62c8113b3bf122c3
micache <huuhoangabcd@gmail.com>
2026-03-29 23:28:27 +0800

    add set api to env config

---
+++ set_api.sh
@@ -0,0 +1,5 @@
+conda env config vars set DRW_GAME_ID=160
+conda env config vars set DRW_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJodXVob2FuZ2FiY2RAZ21haWwuY29tIiwiZXhwIjoxNzc2ODM4MjI4fQ.WLaqAhQpe1OJwzn-cuMfWekKptGFXDbrQ8mBZoVjlNc
+conda env config vars set ODDS_SPORT_KEY="basketball_ncaab"
+conda env config vars set ODDS_API_BASE=https://api.the-odds-api.com/v4
+conda env config vars set ODDS_API_KEY=1888d64241ce027bff7b7462d09bb0da
